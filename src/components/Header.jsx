538008153107083e3dfe9ec11f802166952a4d8c
mmkclement <maungmaungkha18290@gmail.com>
2023-08-29 11:58:05 +0200

    header with reacr splide

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,63 @@
 import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 const Header = () => {
   return (
-    <section className="w-full h-screen">
-      <div className="w-full h-full relative">
-        <img src="https://th.bing.com/th/id/R.b49d646094af34b3ffba16a21d8498b2?rik=AgHbt22%2fZZXVAQ&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f06%2fAmazing-HD-Backgrounds-1920x1080.jpg&ehk=DwXjBT7LIy1K%2baWmByIfeLZnha%2bulu7fLUa%2f4fHtW5U%3d&risl=&pid=ImgRaw&r=0"
-          alt=""
-          className="w-full h-full object-cover"
-        />
-        <div className="dark-ov"></div>
-      </div>
+    <section className="h-screen w-full">
+      <Splide
+        options={{
+          type: "loop",
+          arrows: false,
+          autoplay: true,
+          interval: 2000,
+        }}
+      >
+        <SplideSlide>
+          <div className="w-full h-screen relative">
+            <img
+              src="https://th.bing.com/th/id/R.eb879f435bcf587d7bc1957df1374164?rik=5VIIMOdMRWOS6Q&pid=ImgRaw&r=0"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+            <div className="dark-ov"></div>
+            <div className="text-box">
+              <h1 className="text-5xl font-extra-bold">World is with you</h1>
+              <p className="text-lg font-medium">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                laborum molestias eveniet porro.Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Nobis laborum molestias eveniet
+                porro.
+              </p>
+              <button className="bg-white text-black px-3 py-2 rounded-md font-medium text-lg mt-1">
+                Watch Now
+              </button>
+            </div>
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className="w-full h-screeen relative">
+            <img
+              src="https://th.bing.com/th/id/OIP.TevMNp6ERazATLzjaUekLQHaFG?pid=ImgDet&rs=1"
+              alt=""
+              className="w-full h-full object-cover"
+            />
+            <div className="dark-ov"></div>
+            <div className="text-box">
+              <h1 className="text-5xl font-extra-bold">World is with you</h1>
+              <p className="text-lg font-medium">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                laborum molestias eveniet porro.Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Nobis laborum molestias eveniet
+                porro.
+              </p>
+              <button className="bg-white text-black px-3 py-2 rounded-md font-medium text-lg mt-1">
+                Watch Now
+              </button>
+            </div>
+          </div>
+        </SplideSlide>
+      </Splide>
     </section>
   );
 };
